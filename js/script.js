5bf3f12f98c3ff9c78d713d95b50f2bc157f1416
Yana Rozova <yana.rozova@mail.ru>
2021-08-12 16:57:54 +0300

    Style chenged

--- js/script.js
+++ js/script.js
@@ -1,28 +1,53 @@
-/* slider buttons */
+/* -------slider buttons------- */
+/* slider bullets */
+const sliderControls=document.querySelector(".slider-controls");
+const sliderItems=document.querySelectorAll(".slider-item");
+let addNewElement=function(elem, cls, motherElem){
+	let newElem=document.createElement(elem);
+	newElem.classList.add(cls);
+	motherElem.appendChild(newElem);
+}
+addNewElement("ul", "slider-bullets-list", sliderControls);
+const bulletsList=document.querySelector(".slider-bullets-list");
+for(let numBullets=0; numBullets < sliderItems.length; numBullets++){
+	addNewElement("li","slider-bullet-item",bulletsList);
+}
+console.log(sliderControls);
+/* slider button next and prev*/
 const buttonPrev = document.querySelector(".button-prev");
 const buttonNext = document.querySelector(".button-next");
-const slider = document.querySelectorAll(".slider-img");
-const sliderBullets = document.querySelectorAll(".slider-bullet");
-let index = 0;
-buttonNext.addEventListener("click", function () {
-	if (index === slider.length - 1) {
-		sliderBullets[index].checked = false;
-		sliderBullets[0].checked = true;
-		index = 0;
+const slider = document.querySelectorAll(".slider-item");
+const sliderBullets = document.querySelectorAll(".slider-bullet-item");
+let addActive=function(slideIndex){
+	slider[slideIndex].classList.add("active");
+	sliderBullets[slideIndex].classList.add("active-bullet");
+}
+let removeActive=function(slideIndex){
+	slider[slideIndex].classList.remove("active");
+	sliderBullets[slideIndex].classList.remove("active-bullet");
+}
+let slideIndex=0;
+slider[slideIndex].classList.add("active");
+sliderBullets[slideIndex].classList.add("active-bullet");
+buttonNext.addEventListener('click',function(){
+	if(slideIndex===sliderItems.length-1){
+		removeActive(slideIndex);
+		slideIndex=0;
+		addActive(slideIndex);
 		return;
 	}
-	sliderBullets[index].checked = false;
-	index++;
-	sliderBullets[index].checked = true;
-});
+	removeActive(slideIndex);
+	slideIndex++;
+	addActive(slideIndex);
+	});
 buttonPrev.addEventListener("click", function () {
-	if (index === 0) {
-		sliderBullets[0].checked = false;
-		sliderBullets[slider.length - 1].checked = true;
-		index = slider.length - 1;
+	if (slideIndex === 0) {
+		removeActive(slideIndex);
+		slideIndex = sliderItems.length-1;
+		addActive(slideIndex);
 		return;
 	}
-	sliderBullets[index].checked = false;
-	index--;
-	sliderBullets[index].checked = true;
+	removeActive(slideIndex);
+	slideIndex--;
+	addActive(slideIndex);
 });
