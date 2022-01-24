002e4d6fe0178329a593e4a073f818e89144ac84
Yana Rozova <yana.rozova@mail.ru>
2022-01-24 18:05:59 +0300

    swipper settings started

--- source/js/slider-settings.js
+++ source/js/slider-settings.js
@@ -1,3 +1,18 @@
-import Swiper, { Navigation, Pagination } from 'swiper';
+import Swiper, { Navigation, pagination } from 'swiper';
 
-new Swiper(".swiper");
+const sliderImage = new Swiper(".slider-image", {
+  slidesPerView:1,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+const sliderDesciption = new Swiper(".slider-description", {
+  slidesPerView:1,
+  nested: true,
+});
+
+sliderImage.controller.control = sliderDesciption;
+sliderDesciption.controller.control = sliderImage;
+
