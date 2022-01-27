e1c70b896be422e44ab53956681235f30e4d4c5f
Yana Rozova <yana.rozova@mail.ru>
2022-01-27 17:27:47 +0300

    page setting when burger open

--- source/js/burger.js
+++ source/js/burger.js
@@ -4,27 +4,53 @@ const header = document.querySelector(".header");
 const burger = document.querySelector(".navigation__toggle");
 const navigationItems = document.querySelectorAll(".navigation__item");
 const page = document.querySelector(".page");
+const body = document.querySelector(".body");
+const main = document.querySelector(".main");
 
 navigation.classList.remove("navigation--nojs");
 page.classList.remove("page--nojs");
 
-burger.addEventListener ("click", function(){
-  if (navigation.classList.contains("navigation--closed")) {
-    navigation.classList.remove("navigation--closed");
-    navigation.classList.add("navigation--opened");
-  } else {
-    navigation.classList.add("navigation--closed");
-    navigation.classList.remove("navigation--opened");
-  }
-});
+const openBurger = () => {
+  navigation.classList.remove("navigation--closed");
+  navigation.classList.add("navigation--opened");
+};
 
-const closeBurger = (item) => {
+const closeBurger = () => {
+  navigation.classList.add("navigation--closed");
+  navigation.classList.remove("navigation--opened");
+};
+
+const closeBurgerAfterClick = (item) => {
   item.addEventListener("click", () => {
-    navigation.classList.add("navigation--closed");
-    navigation.classList.remove("navigation--opened");
+    closeBurger();
   })
 };
 
-navigationItems.forEach((item)=>{
-  closeBurger(item);
+burger.addEventListener("click", function () {
+  if (navigation.classList.contains("navigation--closed")) {
+    openBurger();
+    body.classList.add("lock");
+  } else {
+    closeBurger();
+    body.classList.remove("lock");
+  }
+});
+
+main.addEventListener("click", () => {
+  if (navigation.classList.contains("navigation--opened")) {
+    closeBurger();
+    body.classList.remove("lock");
+  }
+})
+
+
+// {
+//body.addEventListener("click", () => {
+//closeBurger();
+//body.classList.remove("lock");
+//})
+//};
+
+navigationItems.forEach((item) => {
+  closeBurgerAfterClick(item);
 })
