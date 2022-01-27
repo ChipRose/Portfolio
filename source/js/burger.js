10effead1206fd66d6a87384d33dca402c315e96
Yana Rozova <yana.rozova@mail.ru>
2022-01-27 16:38:20 +0300

    burger fixed

--- source/js/burger.js
+++ source/js/burger.js
@@ -3,10 +3,10 @@ const navigation = document.querySelector(".navigation");
 const header = document.querySelector(".header");
 const burger = document.querySelector(".navigation__toggle");
 const navigationItems = document.querySelectorAll(".navigation__item");
-const pageBody = document.querySelector(".page__body");
+const page = document.querySelector(".page");
 
 navigation.classList.remove("navigation--nojs");
-navigation.classList.remove("header--nojs");
+page.classList.remove("page--nojs");
 
 burger.addEventListener ("click", function(){
   if (navigation.classList.contains("navigation--closed")) {
