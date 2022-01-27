752c9fa670c61cb0c71f82c55cf367add6f3c6d1
Yana Rozova <yana.rozova@mail.ru>
2022-01-27 13:28:29 +0300

    burger working problems fixed. Slider pdgination fixed

--- source/js/burger.js
+++ source/js/burger.js
@@ -1,10 +1,12 @@
 //Burger
 const navigation = document.querySelector(".navigation");
+const header = document.querySelector(".header");
 const burger = document.querySelector(".navigation__toggle");
 const navigationItems = document.querySelectorAll(".navigation__item");
 const pageBody = document.querySelector(".page__body");
 
 navigation.classList.remove("navigation--nojs");
+navigation.classList.remove("header--nojs");
 
 burger.addEventListener ("click", function(){
   if (navigation.classList.contains("navigation--closed")) {
